07e9963de9c3192b46def54c54c9d67c7eb483d0
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2021-11-07 12:55:17 +0530

    initial commit

--- mock-1/script.js
+++ mock-1/script.js
@@ -1,28 +1,35 @@
-const containerDiv = document.querySelector(".container");
-const profileImg = document.querySelector('#avatar');
-const repoList = document.querySelector('#repo');
+const avatar_img = document.querySelector("#avatar-image");
+const userName = document.querySelector("#user-name");
+const repoCount = document.querySelector("#no-of-repos");
+const ul = document.querySelector("#ul");
 
-document.addEventListener('DOMContentLoaded', getUserData('ArjunGTX'))
-function getUserData(userName) {
-    fetch(`https://api.github.com/users/${userName}`)
-.then(response => response.json())
-.then(data => {
-    
-    profileImg.src = data.avatar_url;
-    // repoList.innerHTML = ``
-    // console.log(data.repos_url);
-    fetch(data.repos_url)
-    .then(repo => repo.json())
-    .then(repoData => {
-        
-        repoData.forEach(element => {
-            let listItem = document.createElement('li');
-            listItem.innerText = element.name;
-           repoList.appendChild(listItem)
-    
+function fetchApi(username = "Avin008") {
+  fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.avatar_url);
+      avatar_img.setAttribute("src", data.avatar_url);
+      userName.innerText = data.name;
+      repoCount.innerText = `No of Repos: ${data.public_repos}`;
+
+      fetch(data.repos_url)
+        .then((repoData) => repoData.json())
+        .then((repos) => {
+          repos.forEach((list) => {
+            console.log(list.name);
+            createElement("li", ul, list.name);
+            return repos;
+          });
+          return data;
         });
     })
-})
+    .catch((err) => console.log("something went wrong"));
 }
 
+function createElement(elName, parent, value) {
+  let element = document.createElement(elName);
+  parent.appendChild(element);
+  element.innerText = value;
+}
 
+fetchApi();
